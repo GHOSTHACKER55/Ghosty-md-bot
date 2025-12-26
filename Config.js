ac54633d24913bcea763ac06e237c9fd3268aebc
GHOSTHACKER55 <musaandanas963@gmail.com>
2025-12-26 16:17:54 +0500

    Config.js
    En

---
+++ Config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // Telegram group (must join)
+  telegramGroup: "https://t.me/CyropesTricks",
+
+  // WhatsApp group (must join)
+  whatsappGroup: "https://chat.whatsapp.com/HVM9Spvm7HVGLERj8yo9kS",
+
+  // WhatsApp channel (shown in EVERY message)
+  whatsappChannel: "https://whatsapp.com/channel/0029VbBuO1IK5",
+
+  // Admin / log channel (BOT MUST BE ADMIN)
+  logChannelId: "@YOUR_LOG_CHANNEL",
+
+  // Ghosty MD Bot image (LOCAL FILE)
+  botImage: "./ghosty-md.png"
+};
