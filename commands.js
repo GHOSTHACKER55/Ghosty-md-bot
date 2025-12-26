104ff0a97bb0e4b4a40a3739f71c473421ed8225
GHOSTHACKER55 <musaandanas963@gmail.com>
2025-12-26 17:42:16 +0500

    Create commands.js

---
+++ commands.js
@@ -0,0 +1,30 @@
+// commands.js
+module.exports = {
+  list: `
+*Ghosty MD Bot – Full Commands List*
+
+*Verification & Membership:*
+.start, .joincheck, .status, .pair +<number>, .unpair +<number>, .verified
+
+*Media & Entertainment:*
+.play <song name>, .download <YouTube URL>, .downloadmp3 <YouTube URL>, .song <song name>, 
+.tiktok <TikTok URL>, .instagram <IG URL>, .facebook <FB URL>, .twitter <Twitter URL>, 
+.soundcloud <SC URL>, .pinterest <Pinterest URL>, .drama <drama name>, .playlist <YouTube playlist URL>
+
+*Utility & Productivity:*
+.qr <text/link>, .sticker <image>, .toimg <sticker>, .translate <lang> <text>, .weather <city>, 
+.news <topic>, .shorten <URL>, .calc <expression>, .remind <time> <message>, 
+.timer <minutes> <message>, .poll <question> <options>
+
+*Fun & Interactive:*
+.meme, .joke, .quote, .avatar <username>, .insult, .fact, .coinflip, .dice, 
+.randomnumber <min> <max>, .rps <rock/paper/scissors>
+
+*AI & Chat:*
+.chat <message>, .imagine <prompt>, .summary <text>, .translateai <lang> <text>
+
+*Admin & Moderation:*
+.logs, .broadcast <message>, .restart, .addadmin <user_id>, .removeadmin <user_id>, 
+.ban <user_id>, .unban <user_id>, .mute <user_id>, .unmute <user_id>
+`
+};
